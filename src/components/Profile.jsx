bb765f42b99649288f5775ad1d70fb0df7eec2ad
walterlbf <walter.lbf.f@gmail.com>
2021-08-31 19:28:39 -0400

    profile refatorado

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -5,10 +5,15 @@ import '../styles/Profile.css';
 export default function Profile() {
   return (
     <section className="profile-section">
-      <img src={profileImage} alt="Profile Icon" />
+      <picture className="profile-picture">
+        <img src={ profileImage } alt="Profile Icon" />
+      </picture>
       <div className='info'>
-        <a className="a" href='/'>Sarah’s day care available now in North Sydney</a>
-        <p className="p">Wednesday, Thursday, Friday - 7:30 - 5:30</p>
+        <a className="profile-a" href='/'>
+          Sarah’s day care available now in North Sydney</a>
+        <p className="profile-p">
+          Wednesday, Thursday, Friday - 7:30 - 5:30
+        </p>
       </div>
     </section>
   )
