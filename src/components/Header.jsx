02184d0a9bc9c0846b4f4a0b0cfc996ebfcab123
walterlbf <walter.lbf.f@gmail.com>
2021-09-01 16:56:40 -0400

    header desktop feito

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,24 +2,30 @@ import React from 'react';
 import '../styles/Header.css';
 import Navbar from './Navbar';
 import playImage from '../img/playButton.svg';
+import headerImage from '../img/ImageHeader.png';
 
 export default function Header() {
   return (
     <>
       <section className="header">
         <Navbar/>
-        <article className="article-header">
-          <h1 className="header-h1">
-            Easily create or join a local nanny share with Hapu
-          </h1>
-          <p className="header-p">
-            Hapu is Airbnb for nanny share. Share your home, nanny and costs and create new flexible, affordable solutions in childcare.
-          </p>
-        </article>
-        <div className="playLink">
-          <img src={ playImage } alt="" />
-          <a id="tag-a-header" href="/">See hapu in action (27 seconds)</a>
-        </div>
+        <section className="flex-section">
+          <article className="article-header">
+            <h1 className="header-h1">
+              Easily create or join a local nanny share with Hapu
+            </h1>
+            <p className="header-p">
+              Hapu is Airbnb for nanny share. Share your home, nanny and costs and create new flexible, affordable solutions in childcare.
+            </p>
+            <div className="playLink">
+              <img src={ playImage } alt="" />
+              <a id="tag-a-header" href="/">See hapu in action (27 seconds)</a>
+            </div>
+          </article>
+          <picture className="header-picture">
+            <img className="header-img" src={ headerImage } alt="" />
+          </picture>
+        </section>
       </section>
     </>
   )
