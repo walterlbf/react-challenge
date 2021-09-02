14657d869b78cf97b1e6794d64c8006b61f7449c
walterlbf <walter.lbf.f@gmail.com>
2021-09-02 03:10:11 -0400

    identacao

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -7,12 +7,12 @@ export default function News() {
     <section className="news-section">
       <img src={ DailyDiary } alt="" />
       <article>
-          <h2 className="news-title">
-            Coming soon: Nanny Share Daily Diary!
-          </h2>
-          <p className="news-p">
-            With the Hapu daily diary your nanny will be able to update you and your sharers with photos and commentary of the day. You and sharers will receive notifications and you’ll be able to login to view the daily adventures fo your little ones. We can’t wait!
-          </p>
+        <h2 className="news-title">
+          Coming soon: Nanny Share Daily Diary!
+        </h2>
+        <p className="news-p">
+          With the Hapu daily diary your nanny will be able to update you and your sharers with photos and commentary of the day. You and sharers will receive notifications and you’ll be able to login to view the daily adventures fo your little ones. We can’t wait!
+        </p>
       </article>
     </section>
   )
