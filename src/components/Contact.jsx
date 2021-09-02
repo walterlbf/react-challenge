e50582730e7e34ff850641056494e5b235b3a2f9
walterlbf <walter.lbf.f@gmail.com>
2021-09-02 01:44:54 -0400

    provider desktop ok

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,21 +4,21 @@ import '../styles/Contact.css';
 export default function Contact() {
 	return (
 		<section className="contact-section">
-			<article>
-				<h2 className="h2-contact">
-				  Are you a parent without a nanny and looking to share?
-				</h2>
-				<p className="p-contact">
-				  Leave us your name and email and we’ll update you as soon as a share becomes available in your area!
-				</p>
-				<form className="forms">
+      <article>
+        <h2 className="h2-contact">
+          Are you a parent without a nanny and looking to share?
+        </h2>
+        <p className="p-contact">
+          Leave us your name and email and we’ll update you as soon as a share becomes available in your area!
+        </p>
+        <form className="forms">
           <input placeholder="Your name" required />
           <input type="email" placeholder="Your email" required />
           <button className="send-button">
             Send
           </button>
         </form>
-			</article>
+      </article>
 		</section>
 	)
 }
