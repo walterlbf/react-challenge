f62291614156d728300970c5b1a31bd1fbaa3b51
walterlbf <walter.lbf.f@gmail.com>
2021-09-02 02:58:51 -0400

    principles desktop ok

--- src/components/Principles.jsx
+++ src/components/Principles.jsx
@@ -1,20 +1,24 @@
 import React from 'react';
 import '../styles/Principles.css';
+import framework from '../img/ImageSection4.png';
 
 export default function Principles() {
   return (
-    <section className="principles-section">
-      <article>
-          <h2 className="principles-title">
+    <section className="framework-section">
+      <article className="framework-article">
+          <h2 className="framework-title">
             A framework built for the long term
           </h2>
-          <p className="principles-p">
+          <p className="framework-p">
           Childcare is for the long term. And you need a framework you can count on that gives your share long term viability and success. That’s why we’ve defined Hapu around our three tribal principles; clearly defined process, transparency over money and equality of participation.
           </p>
-          <a className="principles-a" href="/">
+          <a href="/">
             Read how Hapu’s tribal background defines our app 
           </a>
       </article>
+      <picture className="framework-picture">
+        <img src={ framework } alt="" />
+      </picture>
     </section>
   )
 }
