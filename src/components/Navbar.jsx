62be45f40cc835b142b8eb7e42e84bf6f47d1cdc
walterlbf <walter.lbf.f@gmail.com>
2021-09-02 11:03:43 -0400

    navbar finalizado, e aparentemente o desafio tambem

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,22 +4,35 @@ import Badge from '../img/Badge.svg';
 
 export default function Navbar() {
 return (
-  <>
-    <nav>
-      <img className="logo" src={Badge} alt="" />
-      {/* <ul className="navbar">
-        <li>
-          <a href="">Create Your Nanny Share</a>
-        </li>
-        <li>
-          <a href="">Browse Share</a>
-        </li>
-        <li>
-          <a href="">Our Story</a>
-        </li>
-      </ul> */}
-      <button className="button-navbar">Become a Nanny Share Host</button>
-    </nav>
-  </>
+  <header>
+    <section className="section-nav">
+      <picture>
+        <img className="logo" src={Badge} alt="" />
+      </picture>
+      <nav className="navbar">
+        <ul className="ul-nav-header">
+          <li>
+            <a className='link-nav' href="/">
+              Create Your Nanny Share
+            </a>
+          </li>
+          <li>
+            <a className='link-nav' href="/">
+              Browse Share
+            </a>
+          </li>
+          <li>
+            <a className='link-nav' href="/">
+              Our Story
+            </a>
+          </li>
+        </ul>
+      </nav>
+      <section className="right-content">
+        <button className="button-navbar">Become a Nanny Share Host</button>
+        <a href="/" className='link-nav'> Sing In </a>
+      </section>
+    </section>
+  </header>
 )
 }
